c1a30071b1ce632e0a6056be26643964e71b215f
Bogdan Chorniy <bogdan.chorniy@gmail.com>
2016-07-23 11:17:29 +0300

    Deleted {action} from route template.

--- ServiceDomain/App_Start/WebApiConfig.cs
+++ ServiceDomain/App_Start/WebApiConfig.cs
@@ -13,7 +13,7 @@ namespace ServiceDomain
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{action}/{id}",
+                routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
