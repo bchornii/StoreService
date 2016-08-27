f900b76dff532d404dd0bc8f9f15eb483a344e20
Bogdan Chorniy <bogdan.chorniy@gmail.com>
2016-08-27 19:12:25 +0300

    Implemented exteption filter. To order controller added several methods for explore action-error handling.

---
+++ ServiceDomain/Filters/ExceptionFilter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http.Filters;
+
+namespace ServiceDomain.Filters
+{
+    // Exception filter could be register in several ways :
+    // - by action
+    // - by controller
+    // - globally
+    public class NotImpExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            if(actionExecutedContext.Exception is NotImplementedException)
+            {
+                actionExecutedContext.Response = new HttpResponseMessage(System.Net.HttpStatusCode.NotImplemented);
+            }
+        }
+    }
+}
