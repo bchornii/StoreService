128bd3a30fa9553bbf20a341d5d5735afdfdd50e
Bogdan Chorniy <bogdan.chorniy@gmail.com>
2016-08-29 22:51:49 +0300

    Added unhandled exception logger.

---
+++ ServiceDomain/Logging/UnhandledExceptionLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Http.ExceptionHandling;
+
+namespace ServiceDomain.Logging
+{
+    public class UnhandledExceptionLogger : ExceptionLogger
+    {
+        public override void Log(ExceptionLoggerContext context)
+        {
+            base.Log(context);
+        }
+        
+        private string RequestToString(HttpRequestMessage request) => 
+            request?.Method.ToString() + " " + request?.RequestUri.ToString();
+    }
+}
